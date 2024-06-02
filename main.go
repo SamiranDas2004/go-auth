04ab1ed9e4517e1e7c345ac303d2bda7da041e0a
Samiran_Das <samiran4209@gmail.com>
2024-06-02 16:01:02 +0530

    push

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func main() {
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
+}
